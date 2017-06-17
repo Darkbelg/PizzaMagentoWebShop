e339314d4f93729639c0e36fe3009d62b5f0a648
Darkbelg <stijn.sagaert@outlook.com>
2017-06-17 23:32:35 +0200

    met magento fundamentals modules GreetingMessage en HelloPage

---
+++ app/code/Learning/GreetingMessage/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn
+ * Date: 14-Jun-17
+ * Time: 21:15
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Learning_GreetingMessage',
+    __DIR__
+);
---
+++ app/code/Learning/HelloPage/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn
+ * Date: 15-Jun-17
+ * Time: 22:31
+ */
+/**
+ * Copyright © 2016 Magento. All rights reserved. * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Learning_HelloPage',
+    __DIR__
+);
